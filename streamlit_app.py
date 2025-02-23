a29de3de3bb529d4676d52f57501bc3e55e95f61
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-02-23 10:17:17 +0000

    Tamaño Logo Anco 500px

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ parrafo("¿Podrías contestar nuestro cuestionario?")
 enlace("https://technovationchallenge.org/", "Enlace al cuestionario")
 
 parrafo("__Muchas Gracias.__")
-st.image("./images/Logo-Circular-WEB_OK.png")
+st.image("./images/Logo-Circular-WEB_OK.png", width=500)
