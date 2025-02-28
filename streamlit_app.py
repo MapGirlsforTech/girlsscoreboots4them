c8706f68bda0ab6d3ba72ec303775345800fe61b
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-02-28 16:26:40 +0000

    centrar

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,6 +99,6 @@ parrafo(" ")
 col1, col2, col3 = st.columns([1, 2, 1])  # Tres columnas, el centro tiene el doble de peso
 # Usar la columna central para colocar la imagen
 with col2:
-    st.image("./images/Logo-Circular-WEB_OK.png", use_column_width=True)
+    st.image("./images/Logo-Circular-WEB_OK.png", use_container_width=True)
 
 # st.image("./images/Logo-Circular-WEB_OK.png", width=400)
