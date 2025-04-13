ad5852838c8b0e61be947f5f82299c885f715ebd
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-13 15:11:53 +0000

    titulo en ods

--- sections/ods.py
+++ sections/ods.py
@@ -1,4 +1,10 @@
 import streamlit as st
 from streamlit_app import translate
 
+# Función para añadir un párrafo recibiendo un texto como parametro
+def parrafo(text):
+    st.markdown(text)
+
 st.header(translate("ods"))
+
+parrafo ("17 objetivos para transformar nuestro mundo")
