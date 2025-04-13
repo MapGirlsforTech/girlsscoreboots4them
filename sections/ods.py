03f12a91492c24bc43ba8d60e51d68bef5940da6
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-13 16:11:23 +0000

    ods

--- sections/ods.py
+++ sections/ods.py
@@ -7,4 +7,6 @@ def parrafo(text):
 
 st.header(translate("ods"))
 
-st.markdown("17 objetivos para transformar nuestro mundo")
+parrafo("**17 objetivos para transformar nuestro mundo**")
+parrafo("En 2015, la ONU aprobó la Agenda 2030 sobre el Desarrollo Sostenible, una oportunidad para que los países y sus sociedades emprendieran un nuevo camino con el que mejorar la vida de todas las personas, sin dejar a nadie atrás.")
+parrafo("La Agenda cuenta con 17 Objetivos de Desarrollo Sostenible, que establecen que la erradicación de la pobreza debe ir de la mano de estrategias que fomenten el crecimiento económico y abordan una serie de necesidades sociales como la educación, la sanidad, la protección social y las perspectivas de empleo, al tiempo que se combate el cambio climático y se protege el medio ambiente.")
