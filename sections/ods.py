885b988a4be52f621e5916b02e1094927a8a91c1
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-15 16:10:19 +0000

    estilo

--- sections/ods.py
+++ sections/ods.py
@@ -7,16 +7,22 @@ def parrafo(text):
 
 st.header(translate("ods"))
 
-parrafo("**17 objetivos para transformar nuestro mundo**")
+st.markdown(
+    "<h3 style='text-align: center;'>17 objetivos para transformar nuestro mundo</h3>",
+    unsafe_allow_html=True
+)
+
+#st.markdown("**17 objetivos para transformar nuestro mundo**")
+parrafo("")
 parrafo("En 2015, la ONU aprobó la Agenda 2030 sobre el Desarrollo Sostenible, una oportunidad para que los países y sus sociedades emprendieran un nuevo camino con el que mejorar la vida de todas las personas, sin dejar a nadie atrás.")
 parrafo("La Agenda cuenta con 17 Objetivos de Desarrollo Sostenible, que establecen que la erradicación de la pobreza debe ir de la mano de estrategias que fomenten el crecimiento económico y abordan una serie de necesidades sociales como la educación, la sanidad, la protección social y las perspectivas de empleo, al tiempo que se combate el cambio climático y se protege el medio ambiente.")
-
+parrafo("")
 #Imagen todas las ODS
 
 st.image("./images/Poster ODS_Spanish.png")
-
+parrafo("")
 parrafo("Nuestra App aplica con los siguientes ODS:")
-
+parrafo("")
 #Imagen 4 ODS
 
 # Crear una columna para centrar la imagen
