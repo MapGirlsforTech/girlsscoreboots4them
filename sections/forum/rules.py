3eee8ddad6dd17ed1638ed7bec0ce8e46930a0bc
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-15 16:58:30 +0000

    normas foro

--- sections/forum/rules.py
+++ sections/forum/rules.py
@@ -1,4 +1,19 @@
 import streamlit as st
 from streamlit_app import translate
 
+# Función para añadir un párrafo recibiendo un texto como parametro
+def parrafo(text):
+    st.markdown(text)
+
 st.header(translate("rules"))
+
+
+st.markdown("1. **Promoción del empoderamiento:** Todos los comentarios y publicaciones deben promover el apoyo y el respeto hacia las mujeres en el deporte.")
+st.markdown("2. **Ambiente inclusivo:** Aceptamos y valoramos la diversidad en todas sus formas. Discriminación por género, raza, orientación sexual u otro motivo no será tolerada.")
+st.markdown("3. **Enfoque constructivo:** Las críticas o sugerencias deben ser constructivas y respetuosas. La idea es crecer y aprender juntas.")
+st.markdown("4. **Cuidado con los rumores:** Comparte información verificada y evita difundir rumores o desinformación sobre eventos deportivos o personas.")
+st.markdown("5. **Protección de la salud mental:** Está prohibido cualquier contenido que fomente conductas perjudiciales, como obsesión insana con el peso o comentarios despectivos sobre la imagen corporal.")
+st.markdown("6. **Celebrar los logros:** Comparte historias, logros y momentos inspiradores dentro del deporte. ¡Celebremos juntas cada victoria, grande o pequeña!")
+st.markdown("7. **Espacio seguro:** Se prohíben los ataques personales, los comentarios ofensivos y cualquier tipo de acoso, fomentando un ambiente donde todas se sientan cómodas participando.")
+
+st.markdown("8. **Motivación y recursos:** Comparte ideas, consejos y recursos para que todas puedan mejorar y disfrutar de su práctica deportiva.")
