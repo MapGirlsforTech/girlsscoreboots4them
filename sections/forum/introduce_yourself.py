9d6d6f57f75de1547c2c29605bc80d05511f8c2f
Lucas Rojas <lucas.rojas@live.com.ar>
2025-04-15 17:13:56 -0300

    Create comment card

--- sections/forum/introduce_yourself.py
+++ sections/forum/introduce_yourself.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_app import translate
-from utils import title
+from utils import title, comment_with_avatar
 
 title(translate("introduce_yourself"))
+
+comment_with_avatar("Lucas", "¡Este diseño se ve genial!", "2025-04-15", "https://picsum.photos/400/200")
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+primaryColor = "#4CAF50"  # Color verde
+
 # Función para añadir un párrafo recibiendo un texto como parametro
 def parrafo(text):
     st.markdown(text,unsafe_allow_html=True)
@@ -14,7 +16,7 @@ def buttonLink(label, link):
        f"""
         <div style="display: flex; justify-content: center;">
         <a href="{link}" target="_blank">
-            <button style="background-color: #4CAF50; color: white; padding: 5px 20px; margin-bottom: 10px; font-size: 16px; border: none; cursor: pointer; border-radius: 5px;">
+            <button style="background-color: {primaryColor}; color: white; padding: 5px 20px; margin-bottom: 10px; font-size: 16px; border: none; cursor: pointer; border-radius: 5px;">
                 {label}
             </button>
         </a>
@@ -25,7 +27,7 @@ def buttonLink(label, link):
 
 def title(text):
     st.markdown(f"""
-    <h2 style="text-align: center; color: #4CAF50;">
+    <h2 style="text-align: center; color: {primaryColor};">
         {text}
     </h2>
     """,
@@ -34,9 +36,26 @@ def title(text):
 
 def subtitle(text):
         st.markdown(f"""
-    <h4 style="text-align: center; color: #4CAF50;">
+    <h4 style="text-align: center; color: {primaryColor};">
         {text}
     </h4>
     """,
         unsafe_allow_html=True
+    )
+
+def comment_with_avatar(user, text, date, avatar_url):
+    st.markdown(
+        f"""
+        <div style="display: flex; align-items: center; gap: 10px; padding: 15px; border-radius: 10px; box-shadow: 2px 2px 10px rgba(0,0,0,0.1);background: rgb(38, 39, 48)">
+            <img src="{avatar_url}" width="40px" height="40px" style="border-radius: 50%;" />
+            <div style="display: flex; flex-direction: column;">
+                <div style="display: flex; align-items: center; gap: 5px;">
+                    <strong>{user}</strong> <small style="color: gray;">{date}</small>
+                </div>
+                <div>
+                <p style="margin-top: 5px;">{text}</p>
+                </div>
+            </div>
+        </div>
+        """, unsafe_allow_html=True
     )
