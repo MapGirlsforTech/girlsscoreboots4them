6b059e5c53b409cabcd2d6db093cbe933e1ddc51
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-16 17:27:02 +0000

    parrafo

--- sections/forum/introduce_yourself.py
+++ sections/forum/introduce_yourself.py
@@ -2,6 +2,11 @@ import streamlit as st
 from streamlit_app import translate
 from utils import title, comment_with_avatar
 
+# Función para añadir un párrafo recibiendo un texto como parametro
+def parrafo(text):
+    st.markdown(text)
+
 title(translate("introduce_yourself"))
 
 st.header(translate("introduce_yourself"))
+
