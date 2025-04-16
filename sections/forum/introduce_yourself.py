f913ebe9f54b7dc35987075ec784a9c18eb80316
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-16 15:52:12 +0000

    cambios

--- sections/forum/introduce_yourself.py
+++ sections/forum/introduce_yourself.py
@@ -4,4 +4,4 @@ from utils import title, comment_with_avatar
 
 title(translate("introduce_yourself"))
 
-comment_with_avatar("Lucas", "¡Este diseño se ve genial!", "2025-04-15", "https://picsum.photos/400/200")
+st.header(translate("introduce_yourself"))
