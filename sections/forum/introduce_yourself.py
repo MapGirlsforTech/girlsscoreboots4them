12e2dc595487376a7fd6a4a19d714542a505f54a
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-19 11:50:05 +0000

    presentate

--- sections/forum/introduce_yourself.py
+++ sections/forum/introduce_yourself.py
@@ -1,12 +1,22 @@
 import streamlit as st
 from streamlit_app import translate
-from utils import title, comment_with_avatar
-
-# Función para añadir un párrafo recibiendo un texto como parametro
-def parrafo(text):
-    st.markdown(text)
+from utils import title, comment_with_avatar, parrafo, subtitle
 
 title(translate("introduce_yourself"))
 
 st.header(translate("introduce_yourself"))
 
+subtitle("¡Bienvenida al Foro: Boots for her, Girls Score!!!")
+parrafo("¡Qué alegría tenerte aquí! ")
+parrafo("Este espacio ha sido creado **por mujeres y para mujeres** que comparten una pasión: **el deporte** en todas sus formas.")
+parrafo("Ya seas atleta profesional, amateur, entrenadora, estudiante o simplemente una amante del deporte en todas sus disciplinas, aquí encontrarás una comunidad que te apoya, te inspira y te entiende.")
+parrafo("El deporte no tiene género, pero la industria no tiene en cuenta todas nuestras necesidades, merecemos opciones de calzado deportivo diseñado para nosotras y nuestra anatomía, con la misma tecnología, innovación y variedad que se ofrece a nuestros compañeros masculinos.")
+parrafo("Comparte tus experiencias.")
+parrafo("Intercambia consejos y entrenamientos.")
+parrafo("Habla de nutrición, salud y bienestar.")
+parrafo("Conecta con otras mujeres como tú.")
+parrafo("Aquí celebramos tus logros, aprendemos de los desafíos y crecemos juntas.")
+parrafo("¡Gracias por ser parte de esta comunidad poderosa y auténtica!")
+parrafo("**¡Bienvenida, este es tu espacio!**")
+parrafo("")
+parrafo("")
