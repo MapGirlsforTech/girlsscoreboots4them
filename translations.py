1b0b05fcd8341290dffdd2c5af181f86fecf5f6c
MapGirlsforTech <mapgirlsfortech@gmail.com>
2025-04-24 15:04:54 +0000

    acento

--- translations.py
+++ translations.py
@@ -112,21 +112,8 @@ translations = {
         "merchandising": "Merchandising",
         "experiencies": "Experiencias",
         "footwear_shop": "Tiendas de calzado",
-        "introduce_yourself": {
-            "title": "Presentate",
-            "subtitle": "Bienvenida al Foro: Boots for Her, Girls Score!!!",
-        },
-        "documentation": {
-            "title": "Documentación",
-            "subtitle":"**Datos clave sobre la evolución del calzado deportivo femenino:**",
-            "bullet_1": "1. **Orígenes y primeros modelos**: Aunque el calzado deportivo comenzó a desarrollarse en el siglo XIX, los primeros modelos estaban diseñados exclusivamente para hombres. Las mujeres tenían que adaptar calzado masculino o usar modelos poco adecuados para sus necesidades.",
-            "bullet_2": "2. **Primeras zapatillas deportivas para mujeres**: No fue hasta las décadas de 1920 y 1930 que comenzaron a fabricarse zapatillas deportivas específicamente para mujeres. Sin embargo, la oferta seguía siendo limitada y muchas disciplinas deportivas no contaban con opciones adecuadas.",
-            "bullet_3": "3. **Innovaciones tecnológicas y expansión**: A partir de los años 60, marcas como Adidas y Nike empezaron a desarrollar modelos más especializados para mujeres. Se introdujeron mejoras en amortiguación, soporte y materiales más ligeros.",
-            "bullet_4": "4. **Revolución en los años 70 y 80**: Con el crecimiento del fitness y el running entre las mujeres, las marcas deportivas empezaron a desarrollar modelos con tecnología adaptada a la anatomía femenina. La percepción del calzado deportivo femenino pasó de ser un producto secundario a una necesidad real.",   
-            "bullet_5": "5. **Impacto cultural y social**: La lucha por un calzado deportivo adecuado para mujeres no es solo una cuestión de moda, sino de equidad en el deporte. La falta de opciones refleja una brecha de género en la industria deportiva que aún necesita ser abordada.",
-            "bullet_6": "6. **Desafíos actuales**: A pesar de los avances, muchas disciplinas deportivas aún carecen de opciones de calzado diseñadas específicamente para la anatomía femenina. Esto afecta el rendimiento y la comodidad de las atletas.",
-            "bullet_7": "7. **Empoderamiento y representación**: En las últimas décadas, el calzado deportivo femenino ha sido un símbolo de empoderamiento. Las atletas han exigido mayor representación y opciones diseñadas para su rendimiento, impulsando cambios en la industria.",
-        },
+        "introduce_yourself": "Preséntate",
+        "documentation": "Documentación",
         "questionnaire": "Cuestionario",
         "ods": {
             "title": "ODS",
